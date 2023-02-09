d1adeddf52229267ec0108858d0bda75f9882b73
michael-mb <mbonimichael@yahoo.fr>
2023-02-09 08:11:09 +0100

    update

--- storyblok/multipart/EventDetail.vue
+++ storyblok/multipart/EventDetail.vue
@@ -3,8 +3,11 @@
     <div class="event-info pad-bottom-30">
       <h4>{{blok.title}}</h4>
       <p class="" v-for="item in blok.item">
-        <span class="event-subtitle"><strong>{{item.title}} : </strong></span> <span class="event-organizer-designation"> {{item.value}}</span>
-      </p>
+
+        <span class="event-subtitle"><strong>{{item.title}} : </strong></span>
+        <a :href="item.value" v-if="item.is_link" class="event-organizer-designation"> {{item.value}}</a>
+        <span v-else class="event-organizer-designation"> {{item.value}}</span>
+       </p>
 
     </div><!-- .event-info -->
     <!-- contact-form-7 -->
