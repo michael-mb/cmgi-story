199384c3e6b0c81dfb5b556c555e58e7711e62fa
michael-mb <mbonimichael@yahoo.fr>
2023-02-09 16:24:27 +0100

    update

--- storyblok/multipart/Counter.vue
+++ storyblok/multipart/Counter.vue
@@ -1,9 +1,20 @@
 <template>
   <section class="contact-form-section typo-white section-bg-img o-visible pad-bottom-200" :data-bg="blok.background">
     <div style='position: absolute;width: 100%;height: 100%;background: rgba(0,0,0,0.79); z-index: 0; top: 0'></div>
-    <div class="shape-bottom" data-negative="false" style="z-index: 2;">
+
+    <div class="shape-top ct-hgt" data-negative="false" style="z-index: 2;">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-        <path class="shape-fill" d="M761.9,44.1L643.1,27.2L333.8,98L0,3.8V0l1000,0v3.9"></path>
+        <path class="shape-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
+							c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
+							c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
+      </svg>
+    </div>
+
+    <div class="shape-bottom ct-hgt" data-negative="false" style="z-index: 2;">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+        <path class="shape-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
+							c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
+							c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
       </svg>
     </div>
     <div class="container" style="z-index: 2; position:relative;">
