cc6055f624d16c48f25677340b935ab9877276fa
michael-mb <mbonimichael@yahoo.fr>
2023-02-09 18:19:50 +0100

    update

--- components/MobileHeader.vue
+++ components/MobileHeader.vue
@@ -6,7 +6,7 @@
     <a href="#" class="zmm-close close"></a>
     <div class="zmm-inner bg-white typo-dark">
       <div class="text-center mobile-logo-part margin-bottom-30">
-        <a href="index.html" class="img-before"><img src="/images/logo/logo-mini.png" class="img-fluid" width="70" height="51" alt="Logo"></a>
+        <a href="/" class="img-before"><img src="/images/logo/logo-mini.png" class="img-fluid" width="70" height="51" alt="Logo"></a>
       </div>
       <div class="zmm-main-nav">
       </div>
